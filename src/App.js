34333c72692d89c1a33a5c5702e86a264f574875
fkrhkd507 <ourinon00@gmail.com>
2021-10-29 15:48:11 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-function Food({ name, picture, rating}) {
+function App() {
   return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
-
-// function component에서는 react는 class component의 render함수를 실행한다.
-// 변하는 데이터 state등을 사용하기 위해 class component 사용
-class App extends React.Component {
-  state = {
-    count: 0
-  };
-  add = () => {
-    console.log("add");
-  };
-  minus = () => {
-    console.log("minus");
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
-}
 export default App;
